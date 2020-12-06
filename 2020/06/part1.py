466bbe4e12f18ee3894aad297981df711e8e9638
Joe Constant <joe@joeconstant.com>
2020-12-06 00:23:55 -0700

    2020 - Day 6

---
+++ 2020/06/part1.py
@@ -0,0 +1,17 @@
+def main():
+    groups = []
+    with open("input.txt") as input_file:
+        group = set()
+        for line in input_file:
+            line = line.strip()
+            if line == "":
+                groups.append(group)
+                group = set()
+                continue
+            group = group.union(set(line))
+
+    count = [len(group) for group in groups]
+    print(sum(count))
+
+
+main()
---
+++ 2020/06/part2.py
@@ -0,0 +1,26 @@
+def main():
+    groups = []
+    with open("input.txt") as input_file:
+        group = {"count": 0}
+        for line in input_file:
+            line = line.strip()
+            if line == "":
+                g = [
+                    {question: group[question]}
+                    for question in group
+                    if group[question] == group["count"] and question != "count"
+                ]
+                groups.append(g)
+                group = {"count": 0}
+                continue
+            group["count"] += 1
+            for char in line:
+                if char in group:
+                    group[char] += 1
+                else:
+                    group[char] = 1
+    count = [len(group) for group in groups]
+    print(sum(count))
+
+
+main()
