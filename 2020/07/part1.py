4a523e3d69953854e6400e4015267e9359a15665
Joe Constant <joe@joeconstant.com>
2020-12-07 12:20:40 -0700

    2020 - Day 7

---
+++ 2020/07/part1.py
@@ -0,0 +1,22 @@
+def do_recurse(check_value, lines, poss_bags):
+    bags = [line[0] for line in lines if check_value in line[1]]
+    poss_bags.update(set(bags))
+    return [do_recurse(bag, lines, poss_bags) for bag in bags]
+
+
+def main():
+    lines = []
+    with open("input.txt") as input_file:
+        for line in input_file:
+            line = line.replace(".", "").replace("bags", "").replace("bag", "").strip()
+            rule = line.split("contain")
+            rule[0] = rule[0].strip()
+            rule[1] = rule[1].strip()
+            lines.append(rule)
+
+    poss_bags = set()
+    do_recurse("shiny gold", lines, poss_bags)
+    print(len(poss_bags))
+
+
+main()
---
+++ 2020/07/part2.py
@@ -0,0 +1,28 @@
+def do_recurse(check_value, lines):
+    bags = [line[1] for line in lines if check_value in line[0]]
+    bags = bags.pop()
+    mycount = 0
+    for bag in bags.split(","):
+        if bag != "no other":
+            qty, mybag = bag.strip().split(" ", 1)
+            qty = int(qty)
+            val = do_recurse(mybag, lines)
+            mycount += (val["count"] * qty) + qty
+    return {"bags": [], "count": mycount}
+
+
+def main():
+    lines = []
+    with open("input.txt") as input_file:
+        for line in input_file:
+            line = line.replace(".", "").replace("bags", "").replace("bag", "").strip()
+            rule = line.split("contain")
+            rule[0] = rule[0].strip()
+            rule[1] = rule[1].strip()
+            lines.append(rule)
+
+    ans = do_recurse("shiny gold", lines)
+    print(ans["count"])
+
+
+main()
